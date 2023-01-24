a9f378a27694f8c2aa2506a395478ffdf6cb6c55
Dragan Smoljan <dragan.8.smoljan@gsk.com>
2023-01-24 22:36:56 +0100

    fetched data, and displayed it from the graph cms

---
+++ pages/posts/[slug].js
@@ -0,0 +1,72 @@
+import styles from '@/styles/Slug.module.css'
+import { GraphQLClient, gql } from "graphql-request"
+
+const graphcms = new GraphQLClient('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cld4k3ao304s701um9ooz3li0/master');
+
+const QUERY = gql`
+ query Post ($slug: String!){
+  post (where: {slug: $slub}){
+    id,
+    title,
+    slug,
+    datePublished,
+    author {
+      id,
+      name,
+      avatar{
+        url
+      }
+    }
+    content{
+      html
+    }
+    coverPhoto{
+      id 
+      url
+    }
+  }
+ }
+`;
+
+const SLUGLIST = gql`
+  {
+    post {
+      slug
+    }
+  }
+`;
+
+export async function getStaticPaths(){
+  const posts  = await graphcms.request(SLUGLIST);
+  return {
+    paths: posts.map((post) => ({ params: { slug: post.slug }})),
+    fallback: false
+  }
+};
+
+export async function getStaticProps() {
+  const slug = params.slug;
+  const data = await graphcms.request(QUERY, {slug});
+  const post = data.post;
+  return {
+    props: {
+      post
+    },
+    revalidate: 10
+  }
+}
+
+export default function BlogPost ({post}) {
+    return (
+      <main className={styles.blog}>
+        <img src={post.coverPhoto.url} className={styles.cover} alt=""/>
+        <div className={styles.title}>
+          <img src={post.author.avatar.url} alt=""/>
+          <div className={styles.authtext}>
+            <h6>By {post.author.name}</h6>
+            <h6 className={styles.date}>{post.datePublished}</h6>
+          </div>
+        </div>
+      </main>
+    )
+}
